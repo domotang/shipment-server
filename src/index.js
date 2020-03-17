3fa8bed9b0c17c7428f7d573bdd6b3b326b83636
Stephen Reed <domotang@hotmail.com>
2020-03-17 11:58:03 -0500

    Removes console message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import http from "http";
 import app from "./server";
 
-console.log("hello", process.env.DB_CONNECT_STRING);
-
 var server = http.createServer(app);
 
 server.listen(8000, "0.0.0.0", () => {
